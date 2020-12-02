92b6e8440e445b5fbaf3ccddf3678cfa7f43e9d6
MorganWilliamson <mwilliamson7695@gmail.com>
2020-12-01 18:15:27 -0600

    Managing files after re-reading readme.

--- api/server.js
+++ api/server.js
@@ -7,6 +7,13 @@ const server = express();
 // server.use goes here
 server.use(express.json());
 server.use(cors);
-// server.use('/api/posts-router', postsRouter)
+// server.use('/api/posts/posts-router', postsRouter)
+
+server.get('/', (req, res) => {
+    res.send(`
+        <h1>Node API Posts Project</h1>
+        <p>The posts below are generated using Node.js</p>
+    `)
+})
 
 module.exports = server;
